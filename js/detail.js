059be948c47c3eaa0f9b851571211370a3490308
Binoop MJ <binoop.mj@fayausa.com>
2018-01-24 17:16:24 +0530

    cochin and kozhikode editions added

--- js/detail.js
+++ js/detail.js
@@ -15,8 +15,15 @@ $(function() {
 
       if (f.eventID == eID) {
 
-        var eventdetail = "<div class='video-wrap'><amp-youtube data-videoid='"+ f.youtubeID +"' layout='responsive' width='480' height='270'></amp-youtube></div><div class='event-detail'><h4>"+ f.title +"</h4><span class='speaker-detail speaker'>By: " + f.speaker + "</span><span class='speaker-detail month'>Streamed live on " + f.date + "</span><p class='event-content'>"+ f.details +"</p></div><div class='speaker-wrap'><img class='speaker-thumb' src='/location/"+ location +"/speakers/"+ f.thumb +"'><h3>"+ f.speaker +"</h3><span class='speaker-name'><span class='speaker-detail company'>" + f.profile + "</span></span><div class='details'><p>"+ f.speakerdesc +"</p></div></div>"
-
+        if (f.youtubeID == "") {
+          var bannerdetail = "<div class='video-wrap'><img class='event-banner' src='/location/"+f.location+"/banners/"+ f.eventbanner +"'/></div>"
+        } else {
+          var bannerdetail = "<div class='video-wrap'><amp-youtube data-videoid='"+ f.youtubeID +"' layout='responsive' width='480' height='270'></amp-youtube></div>"
+        }
+        
+        var eventdetail = "<div class='event-detail'><h4>"+ f.title +"</h4><span class='speaker-detail speaker'>By: " + f.speaker + "</span><span class='speaker-detail month'>Streamed live on " + f.date + "</span><p class='event-content'>"+ f.details +"</p></div><div class='speaker-wrap'><img class='speaker-thumb' src='/location/"+ location +"/speakers/"+ f.thumb +"'><h3>"+ f.speaker +"</h3><span class='speaker-name'><span class='speaker-detail company'>" + f.profile + "</span></span><div class='details'><p>"+ f.speakerdesc +"</p></div></div>"
+        
+        $(bannerdetail).appendTo("#event-detail-wrap");
         $(eventdetail).appendTo("#event-detail-wrap");
 
       } 

b0bb6773d7abc398d7ea59495b418cf9f41ea2fc
Binoop MJ <binoop.mj@fayausa.com>
2018-02-19 17:43:55 +0530

    edition number added to listing

--- js/list.js
+++ js/list.js
@@ -8,7 +8,7 @@ $(function() {
 	  	$( "#event-list" ).empty(); // Clearing all the events before the tab content loads
 		$.getJSON('location/'+ loc + '/'+ loc +'.json', function(data) {
 	       $.each(data.event, function(key, value) {
-	          var details = "<li class='loc-cls "+ value.technology +"'><div class='video-widget-wrap'><a href='/detail.html?loc="+loc +"&id="+value.eventID+"'><div class='video-thumb'><amp-img  width='100' height='50' layout='responsive' src='/location/"+loc+"/banners/"+ value.eventbanner +"'></amp-img><div class='video-overlay'></div></div></a><div class='video-detail-wrap'><h4>" + value.title + "</h4><span class='speaker'>" + value.speaker + "</span><span class='company'>" + value.profile + "</span><span class='month'>"+ value.date + "</span></div></div></li>"
+	          var details = "<li class='loc-cls "+ value.technology +"'><div class='video-widget-wrap'><a href='/detail.html?loc="+loc +"&id="+value.eventID+"'><div class='video-thumb'><amp-img  width='100' height='50' layout='responsive' src='/location/"+loc+"/banners/"+ value.eventbanner +"'></amp-img><div class='video-overlay'></div></div></a><div class='video-detail-wrap'><h4>" + value.title + "</h4><span class='speaker'>" + value.speaker + "</span><span class='company'>" + value.profile + "</span><span class='month'>"+ value.date + "</span><span class='edition'>"+ value.eventID + "</span></div></div></li>"
 	           $(details).appendTo("#event-list");
 	           
 	     	});
@@ -22,7 +22,7 @@ $(function() {
    $.getJSON('location/tvm/tvm.json', function(data) {
         $.each(data.event, function(key, value) {
 
-           var details = "<li class='loc-cls "+ value.technology +"'><div class='video-widget-wrap'><a href='/detail.html?loc=tvm&id="+value.eventID+"'><div class='video-thumb'><amp-img  width='100' height='50' layout='responsive' src='/location/tvm/banners/"+ value.eventbanner +"'></amp-img><div class='video-overlay'></div></div></a><div class='video-detail-wrap'><h4>" + value.title + "</h4><span class='speaker'>" + value.speaker + "</span><span class='company'>" + value.profile + "</span><span class='month'>"+ value.date + "</span></div></div></li>"
+           var details = "<li class='loc-cls "+ value.technology +"'><div class='video-widget-wrap'><a href='/detail.html?loc=tvm&id="+value.eventID+"'><div class='video-thumb'><amp-img  width='100' height='50' layout='responsive' src='/location/tvm/banners/"+ value.eventbanner +"'></amp-img><div class='video-overlay'></div></div></a><div class='video-detail-wrap'><h4>" + value.title + "</h4><span class='speaker'>" + value.speaker + "</span><span class='company'>" + value.profile + "</span><span class='month'>"+ value.date + "</span><span class='edition'>"+ value.eventID + "</span></div></div></li>"
            $(details).appendTo("#event-list");
            
 

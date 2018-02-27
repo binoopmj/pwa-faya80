51156803f1808855e84d2b819a589f3a20e1f02d
Binoop MJ <binoop.mj@fayausa.com>
2018-02-27 15:50:02 +0530

    speaker directory modified

--- js/speakers.js
+++ js/speakers.js
@@ -9,7 +9,7 @@ $(function() {
 	    $.each(data.speakers, function(i, sp) {
 	    	
 	    	
-	    	$.getJSON('/location/tvm/tvm.json', function(data) {
+	    	$.getJSON('/location/all/events.json', function(data) {
 
 				var speakerdetail = "<h4>"+ sp.speaker +"</h4><br><span>"+ sp.profile +"</span>"
 				$(speakerdetail).appendTo("#speaker-list");
@@ -19,8 +19,19 @@ $(function() {
 			  		$.each(sp.events, function(j, evnt) {
 							
 			    		if (evnt == ev.eventID) {
-			  				var eventlist = "<a href='detail.html?loc=tvm&id="+ sp.events +"'>"+ ev.title +"</a><br>"
-			    			$(eventlist).appendTo("#speaker-list");
+			    			if (ev.eventID.match("^T")) {
+			    				var eventlist = "<a href='detail.html?loc=tvm&id="+ sp.events[j] +"'>"+ ev.title +"</a><br>"
+			    				$(eventlist).appendTo("#speaker-list");
+							}
+							else if (ev.eventID.match("^C")){
+								var eventlist = "<a href='detail.html?loc=kochi&id="+ sp.events[j] +"'>"+ ev.title +"</a><br>"
+			    				$(eventlist).appendTo("#speaker-list");
+							}
+							else if (ev.eventID.match("^K")) {
+								var eventlist = "<a href='detail.html?loc=kzkd&id="+ sp.events[j] +"'>"+ ev.title +"</a><br>"
+			    				$(eventlist).appendTo("#speaker-list");
+							}
+			  				
 			  			}
 			    	});
 			  	});	 
